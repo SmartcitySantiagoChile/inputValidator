7dd43fbf6648fcc20fe8f1364e5852c90a70e3d3
Bastianleaf <bastian.martinez@ing.uchile.cl>
2020-10-08 11:55:01 -0300

    Add precommit hooks

--- validate_data.py
+++ validate_data.py
@@ -10,10 +10,10 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-INPUTS_PATH = os.path.join(DIR_PATH, 'inputs')
-OUTPUT_PATH = os.path.join(DIR_PATH, 'output')
-OUTPUT_NAME = 'transaccionesPorDia'
-CONFIG = os.path.join(INPUTS_PATH, 'configuration.json')
+INPUTS_PATH = os.path.join(DIR_PATH, "inputs")
+OUTPUT_PATH = os.path.join(DIR_PATH, "output")
+OUTPUT_NAME = "transaccionesPorDia"
+CONFIG = os.path.join(INPUTS_PATH, "configuration.json")
 
 
 def main(argv):
@@ -21,21 +21,23 @@ def main(argv):
     Script to validate Transantiago Data
     """
     f = Figlet()
-    logger.info(f.renderText('Input Validator'))
+    logger.info(f.renderText("Input Validator"))
 
     # Arguments and description
-    parser = argparse.ArgumentParser(description='validate Trasantiago data.')
+    parser = argparse.ArgumentParser(description="validate Trasantiago data.")
 
-    parser.add_argument('path', help='Path for .zip data')
-    parser.add_argument('--output', default=None,
-                        help='path where report will be saved, if it is not provided we will use output path')
+    parser.add_argument("path", help="Path for .zip data")
+    parser.add_argument(
+        "--output",
+        default=None,
+        help="path where report will be saved, if it is not provided we will use output path",
+    )
     args = parser.parse_args(argv[1:])
 
     input_path = args.path
     output_path = args.output if args.output else OUTPUT_PATH
-    print(CONFIG)
-
-
+    print(input_path)
+    print(output_path)
 
 
 if __name__ == "__main__":
