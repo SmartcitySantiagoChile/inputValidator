78a12e7fafa6dd6e6f1e48e1b8f2835b87b40f25
Bastianleaf <bastian.martinez@ing.uchile.cl>
2020-10-09 17:21:59 -0300

    Add check_name functions and tests

--- data_iterator.py
+++ data_iterator.py
@@ -1,9 +1,29 @@
+import glob
 import json
+import os
+
+
+def check_name_file(path, name):
+    real_path = os.path.join(path, name)
+    return os.path.exists(real_path)
+
+
+def check_regex_file(path, regex):
+    return True if len(glob.glob(os.path.join(path, regex))) > 0 else False
+
 
 check_name_functions = {
-    "name": lambda x: True,
-    "regex": lambda x: True,
-    "root": lambda x: True,
+    "name": check_name_file,
+    "regex": lambda x, y: True,
+    "root": lambda x, y: True,
+}
+
+file_errors = {
+    1: {
+        "name": "Nombre incorrecto",
+        "type": "formato",
+        "message": "El nombre del directorio o archivo no está en el formato correcto.",
+    }
 }
 
 
@@ -14,16 +34,20 @@ class DataIterator:
         with open(config_path) as json_config:
             self.config = json.loads(json_config.read())
         self.data_path = data_path
+        self.report_errors = []
         self.report = []
 
     def start_iteration_over_configuration_tree(self):
-        self.iterate_over_configuration_tree(self.config)
-        print(self.report)
+        self.iterate_over_configuration_tree(self.config, "")
 
-    def iterate_over_configuration_tree(self, node):
+    def iterate_over_configuration_tree(self, node, path):
         name = node["path"]["name"]
         type_name = node["path"]["type"]
-        if check_name_functions[type_name](name):
+        new_path = os.path.join(path, name)
+        if check_name_functions[type_name](os.path.join(self.data_path, path), name):
             for child in node.get("children", []):
-                self.iterate_over_configuration_tree(child)
-        self.report.append(node["path"]["name"])
+                self.iterate_over_configuration_tree(child, new_path)
+        else:
+            self.report_errors.append([new_path, file_errors[1]])
+        if name:
+            self.report.append(new_path)
--- tests/test_data_iterator.py
+++ tests/test_data_iterator.py
@@ -1,7 +1,7 @@
 import os
 from unittest import TestCase
 
-from data_iterator import DataIterator
+from data_iterator import DataIterator, check_name_file, check_regex_file
 
 
 class DataIteratorTest(TestCase):
@@ -9,13 +9,37 @@ class DataIteratorTest(TestCase):
         path = os.path.dirname(os.path.realpath(__file__))
         self.input_path = os.path.join(path, "input")
         self.configuration_path = os.path.join(self.input_path, "configuration.json")
+        self.data_path = os.path.join(self.input_path, "data")
+        self.check_name_data_path = os.path.join(self.input_path, "check_name_data")
+
+    def test_check_name_file_folder(self):
+        self.assertTrue(check_name_file(self.check_name_data_path, "Diccionario"))
+
+    def test_check_name_file_file(self):
+        diccionario_comunas_folder = os.path.join(self.check_name_data_path, "")
+        self.assertTrue(diccionario_comunas_folder, "Diccionario-Comunas.csv")
+
+    def test_check_name_file_doesnt_exist(self):
+        self.assertFalse(check_name_file(self.check_name_data_path, "Wrongname"))
+
+    def test_check_regex_file(self):
+        regex = "Diccionario-DetalleServicioZP_*_*.csv"
+        self.assertTrue(
+            check_regex_file(
+                os.path.join(self.check_name_data_path, "Diccionario"), regex
+            )
+        )
+
+    def test_check_regex_file_wrong(self):
+        regex = "Diccionario*Detallel.csv"
+        self.assertFalse(
+            check_regex_file(
+                os.path.join(self.check_name_data_path, "Diccionario"), regex
+            )
+        )
 
     def test_file_iterator(self):
-        file_list = os.path.join(
-            os.path.join(self.input_path, "Original_2020-07 Julio"), "Diccionario"
-        )
-        file_list = os.path.join(file_list, "Diccionario-Comunas.csv")
         data = DataIterator(
-            data_path=file_list, config_path=self.configuration_path, file_list=True
+            data_path=self.data_path, config_path=self.configuration_path
         )
         data.start_iteration_over_configuration_tree()
