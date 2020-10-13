ad222742e027fd6988e510ddcdeedbe4880b71a8
Bastianleaf <bastian.martinez@ing.uchile.cl>
2020-10-13 13:26:03 -0300

    Add data_validator structure:  - create dispatch_rules structure  - adjust configuration file  - create tests

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # inputValidator
+[![Imports: isort](https://img.shields.io/badge/%20imports-isort-%231674b1?style=flat&labelColor=ef8336)](https://pycqa.github.io/isort/)
 To check consistency in input files
--- tests/test_data_validator.py
+++ tests/test_data_validator.py
@@ -1,7 +1,8 @@
+import json
 import os
 from unittest import TestCase
 
-from data_validator import DataValidator, check_name_file, check_regex_file
+from data_validator import DataValidator, check_name_file, check_regex_file, min_rows
 
 
 class DataValidatorTest(TestCase):
@@ -11,6 +12,8 @@ class DataValidatorTest(TestCase):
         self.configuration_path = os.path.join(self.input_path, "configuration.json")
         self.data_path = os.path.join(self.input_path, "data")
         self.check_name_data_path = os.path.join(self.input_path, "check_name_data")
+        with open(self.configuration_path) as json_config:
+            self.configuration_file = json.loads(json_config.read())
 
     def test_check_name_file_folder(self):
         self.assertTrue(check_name_file(self.check_name_data_path, "Diccionario"))
@@ -38,8 +41,37 @@ class DataValidatorTest(TestCase):
             )
         )
 
-    def test_file_iterator(self):
-        data = DataValidator(
-            data_path=self.data_path, config_path=self.configuration_path
+    def test_min_rows(self):
+        self.assertEqual((2, True), min_rows(1, [], 1))
+        self.assertEqual((2, False), min_rows(1, [], 3))
+
+    def test_dispatch_rules(self):
+        data_validator = DataValidator(self.configuration_path, self.data_path)
+        configuration_path = os.path.join(
+            self.input_path, "configuration_dispatch.json"
         )
-        data.start_iteration_over_configuration_tree()
+        with open(configuration_path) as json_config:
+            configuration_file = json.loads(json_config.read())
+            rules = configuration_file["children"][0]["children"][0]["rules"]
+            expected_dict = {"count": [{"fun": min_rows, "args": 43, "error": 2}]}
+            self.assertEqual(expected_dict, data_validator.dispatch_rules(rules))
+
+    # def test_file_iterator(self):
+    #    data = DataValidator(
+    #        data_path=self.data_path, config_path=self.configuration_path
+    #    )
+    #    data.start_iteration_over_configuration_tree()
+
+    def test_validate_nodes_rules_empty_case(self):
+        data_validator = DataValidator(self.configuration_path, self.data_path)
+        path = self.data_path
+        name = self.configuration_file["path"]["name"]
+        rules = self.configuration_file["rules"]
+        self.assertEqual([], data_validator.validate_node_rules(path, name, rules))
+
+    def test_validate_nodes_rules_format_rule(self):
+        data_validator = DataValidator(self.configuration_path, self.data_path)
+        path = self.data_path
+        name = self.configuration_file["path"]["name"]
+        rules = self.configuration_file["rules"]
+        self.assertEqual([], data_validator.validate_node_rules(path, name, rules))
